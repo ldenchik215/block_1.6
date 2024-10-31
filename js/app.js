27eb6ea57c7f0652b9faef9a8cd5c0e1cee96552
Denis <ldenchik215@gmail.com>
2024-11-01 00:00:31 +0700

    fix js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 const brandsBtn = document.getElementById('show-all-btn-brands')
 const cardBradsList = document.querySelector('.cards-brands')
 
-
 brandsBtn.addEventListener('click', () => {
 	brandsBtn.classList.toggle('read-more-btn--open')
 	cardBradsList.classList.toggle('cards-brands--show-all')
+
+	if (brandsBtn.innerText === 'Показать все') {
+		brandsBtn.innerText = 'Скрыть'
+	} else {
+		brandsBtn.innerText = 'Показать все'
+	}
 })
